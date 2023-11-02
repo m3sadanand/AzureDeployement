66d7b4bb4481380150cf79fc0dcf6deda1ee8645
m3sadanand <mahabaleshwar3sadanand@gmail.com>
2023-11-03 01:06:31 +0530

    Initial commit

---
+++ src/main/java/com/azure/deployement/AzureDeployement/AzureDeployementApplication.java
@@ -0,0 +1,21 @@
+package com.azure.deployement.AzureDeployement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureDeployementApplication {
+
+	@GetMapping("/hello")
+	public String displayMessage(){
+		return "Container deployed to Azure Successfully ";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDeployementApplication.class, args);
+	}
+
+}
