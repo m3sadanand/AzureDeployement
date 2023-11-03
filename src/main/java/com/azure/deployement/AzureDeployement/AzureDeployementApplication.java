fb08d2551d16d718eb725d9272f084addaa89387
Mahabaleshwar Sadanand <mahabaleshwar3sadanand@gmail.com>
2023-11-03 08:25:26 +0530

    Update AzureDeployementApplication.java

--- src/main/java/com/azure/deployement/AzureDeployement/AzureDeployementApplication.java
+++ src/main/java/com/azure/deployement/AzureDeployement/AzureDeployementApplication.java
@@ -11,7 +11,7 @@ public class AzureDeployementApplication {
 
 	@GetMapping("/hello")
 	public String displayMessage(){
-		return "Container deployed to Azure Successfully Now";
+		return "Container deployed to Azure Successfully";
 	}
 
 	public static void main(String[] args) {
